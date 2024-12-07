83ba288d4c7a46d4f8bfc1e60459fda1d7b233be
hoseinaghaei <h.aghaei.araei@gmail.com>
2024-12-07 23:35:18 +0330

    add news section.

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,39 +4,66 @@ import {Link} from 'react-router-dom';
 
 
 import "./Page.css";
+import hamlet from '../file/HAMLET.pdf';
+
 
 class Page extends Component {
   render() {
     return (
-      <div className="bio">
-        <div>
-          <h2>Bio</h2>
-          <p>My name is <b>Hossein Aghaei</b> (حسین آقائی in Persian);
-            <br/>
-            I am a senior bachelor student in the computer engineering department
-            at Sharif University of Technology (SUT) with a total GPA of <b>19.13/20 (19.83 in 2 last years)</b>.
-            <br/>
-            My research interests are <b> Database Management & Software Engineering & Distributed Systems.
-            </b>
-            <br/>
-            For my Bachelor's thesis, I am working on <b>IoTDB (a new leading time-series database)</b> under
-            the supervision by Prof. <a href="http://www.im-creator.com/free/bardiasafaei/homepage">Bardia
-              Safaei</a>.
-          </p>
-          {/*<p>*/}
-          {/*  */}
-          {/*</p>*/}
+      <div>
+        <div className="bio">
+          <div>
+            <h2>Bio</h2>
+            <p>My name is <b>Hossein Aghaei</b> (حسین آقائی in Persian);
+              <br/>
+              I am a senior bachelor student in the computer engineering department
+              at Sharif University of Technology (SUT) with a total GPA of <b>19.13/20 (19.83 in 2 last years)</b>.
+              <br/>
+              My research interests are <b> Database Management & Software Engineering & Distributed Systems.
+              </b>
+              <br/>
+              For my Bachelor's thesis, I am working on <b>IoTDB (a new leading time-series database)</b> under
+              the supervision by Prof. <a href="http://www.im-creator.com/free/bardiasafaei/homepage">Bardia
+                Safaei</a>.
+            </p>
+            {/*<p>*/}
+            {/*  */}
+            {/*</p>*/}
 
-          For more information see <Link to="/exp">experience</Link>
+            For more information see <Link to="/exp">experience</Link>
+
+          </div>
+
+          <div className="map">
+            <script type="text/javascript" id="clustrmaps"
+                    src="//clustrmaps.com/map_v2.js?d=BoOk5rTKBwGrVz_1s7lmgwhEZ5Zcg_vpGzwCf-hKBtE&cl=ffffff&w=a"></script>
+          </div>
 
         </div>
 
-        <div className="map">
-          <script type="text/javascript" id="clustrmaps"
-                  src="//clustrmaps.com/map_v2.js?d=BoOk5rTKBwGrVz_1s7lmgwhEZ5Zcg_vpGzwCf-hKBtE&cl=ffffff&w=a"></script>
+        <div className="bio">
+          <div>
+            <h2>News</h2>
+            <li>
+              <i>
+                Nov-2024: Our paper <b>
+                <a href={hamlet}> HAMLET </a>
+              </b>
+                has been submitted to DAC-2025.
+              </i>
+            </li>
+
+          </div>
+
+          <div className="map">
+            <script type="text/javascript" id="clustrmaps"
+                    src="//clustrmaps.com/map_v2.js?d=BoOk5rTKBwGrVz_1s7lmgwhEZ5Zcg_vpGzwCf-hKBtE&cl=ffffff&w=a"></script>
+          </div>
+
         </div>
 
       </div>
+
 
     );
   }
